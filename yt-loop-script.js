f986c46edcd000611bce19f9667e7e24d50e255a
morgankar <realrohankar03@gmail.com>
2025-01-09 19:44:32 +0530

    Adding button in the website

--- yt-loop-script.js
+++ yt-loop-script.js
@@ -1,2 +1,16 @@
 //YtLoopExtension script
 
+let controlBar = document.querySelector("yt-spec-button-view-model style-scope ytd-menu-renderer")
+
+if (controlBar) {
+    // Proceed to add your button
+    const loopButton = document.createElement("button");
+    loopButton.innerText = "Loop"
+    controlBar.appendChild(loopButton)
+
+} else {
+    console.error("Control bar not found!");
+}
+
+
+
