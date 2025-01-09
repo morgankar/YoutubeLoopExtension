9d3a1f395430e2be128625d1e5e139bbf129f46a
morgankar <realrohankar03@gmail.com>
2025-01-09 20:16:01 +0530

    button not appearing-maybe element chosen is wrong

--- yt-loop-script.js
+++ yt-loop-script.js
@@ -1,6 +1,8 @@
 //YtLoopExtension script
+console.log("Hii")
 
-let controlBar = document.querySelector("yt-spec-button-view-model style-scope ytd-menu-renderer")
+let controlBar = document.querySelector("yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading")
+
 
 if (controlBar) {
     // Proceed to add your button
